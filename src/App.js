c70c7fd48aba38e87a33c3dda0d1949d1ede9f65
jaisy <miemail@dominio.com>
2023-06-09 09:14:50 -0500

    Buscador casi completo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BarraBusqueda from "./components/BarraBusqueda";
 
 const people = [
   {
@@ -59,6 +60,7 @@ function App() {
 
   const [selection, setSelection] = useState(null);
   const [option, setOption] = useState('all');
+  const [ cont, setCont] = useState(1);
 
   function handleClick(e) {
     const key = e.target.name;
@@ -91,11 +93,10 @@ function App() {
       <button onClick={handleClick} name="people"> Personas </button>
       <button onClick={handleClick} name="calendar"> Calendario </button>
       <button onClick={handleClick} name="email"> Asunto Correo </button>
-      <div>
-        {data.map((item)=>(
-          <div>{item.title}</div>
-          ))}
-      </div>
+      <button onClick={()=>{setCont(cont+1)}}>{cont}</button>
+
+      <BarraBusqueda items={data}
+                    itemSeleccionados={()=>{}}/>
       
     </div>
   );
--- src/components/BarraBusqueda.js
+++ src/components/BarraBusqueda.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Resultados from "./Resultados";
 
 export default function BarraBusqueda({items,itemSeleccionados}){
     const [consulta, setConsulta ]=useState("");
@@ -8,9 +9,17 @@ export default function BarraBusqueda({items,itemSeleccionados}){
         const value=e.target.value;
         setConsulta(value);
     }
-
-    return
+    function handleResults(items){
+        setResultados(items)
+    }
+    return(
     <div>
+        {resultados&&<div>{resultados.length}results</div>}
         <input type="text" onChange={handleChange} value={consulta}/>
+        <Resultados items={items}
+                    itemSeleccionados={()=>{}}
+                    consulta={consulta} 
+                    resulCalculados={handleResults}/>
     </div>
+    )
 }
